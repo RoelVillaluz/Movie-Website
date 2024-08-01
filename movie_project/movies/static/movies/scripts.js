2a122d40fdae8eb76273f6b065928fbb4d0b4c0a
RoelVillaluz <roelo.villaluz@gmail.com>
2024-08-01 13:59:04 +0800

    Updated animations
    - added js to make movies alternate animations

--- movie_project/movies/static/movies/scripts.js
+++ movie_project/movies/static/movies/scripts.js
@@ -6,3 +6,65 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const movieData = [
+      {
+          title: 'Spider-Man',
+          subtitle: 'Across the Spiderverse',
+          image: 'media/media/backdrop_VyAOcwH.jpg',
+          link: 'movies/155'
+      },
+      {
+          title: 'The Batman',
+          subtitle: '',
+          image: 'media/media/the_batman_2022_backdrop.jpg',
+          link: 'movies/180'
+      },
+      {
+        title: 'Minions',
+        subtitle: 'The Rise of Gru',
+        image: 'media/media/backdrop_VJeYqoT.jpg',
+        link: 'movies/179'
+    },
+  ];
+
+  let currentIndex = 0;
+
+  const imgElement = document.querySelector('.featured-movie img');
+  const titleElement = document.getElementById('movie-title');
+  const subTitleElement = document.getElementById('movie-subtitle');
+  const linkElement = document.getElementById('movie-link');
+
+  function updateMovie() {
+      currentIndex = (currentIndex + 1) % movieData.length;
+      const movie = movieData[currentIndex];
+      
+      imgElement.classList.remove('animate-img');
+      titleElement.classList.remove('animate-title');
+      subTitleElement.classList.remove('animate-subtitle');
+      
+      // Trigger reflow to restart animations
+      void imgElement.offsetWidth; // Trigger reflow
+      void titleElement.offsetWidth; // Trigger reflow
+      void subTitleElement.offsetWidth; // Trigger reflow
+
+      imgElement.classList.add('animate-img');
+      titleElement.classList.add('animate-title');
+      subTitleElement.classList.add('animate-subtitle');
+      
+      imgElement.src = movie.image;
+      titleElement.textContent = movie.title;
+      subTitleElement.textContent = movie.subtitle;
+      linkElement.href = movie.link;
+  }
+
+  function startMovieRotation() {
+      imgElement.classList.add('animate-img');
+      titleElement.classList.add('animate-title');
+      subTitleElement.classList.add('animate-subtitle');
+      imgElement.addEventListener('animationend', updateMovie);
+  }
+
+  startMovieRotation();
+});
