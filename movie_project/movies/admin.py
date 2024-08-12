4eb39e7a51bae6193f802f5069aaaab945dbfc82
RoelVillaluz <roelo.villaluz@gmail.com>
2024-08-12 17:29:08 +0800

    added inline in movie and actor model admins

--- movie_project/movies/admin.py
+++ movie_project/movies/admin.py
@@ -4,7 +4,6 @@ from .models import Actor, Award, Director, Movie, Genre, MovieImage, MovieVideo
 
 # Register your models here.
 admin.site.register(Genre)
-admin.site.register(Actor)
 admin.site.register(Director)
 admin.site.register(User)
 admin.site.register(MovieImage)
@@ -25,12 +24,22 @@ class ReleaseYearListFilter(admin.SimpleListFilter):
         if self.value():
             return queryset.filter(release_date__year=self.value())
         return queryset
+    
+class MovieInline(admin.TabularInline):
+    model = Actor.movies.through
+    extra = 1
+
+class ActorInline(admin.TabularInline):
+    model = Movie.actors.through
+    extra = 1
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('title', 'display_genres')
     search_fields = ('title', 'genres__name')
     list_filter = ('genres', ReleaseYearListFilter)
+    inlines = [ActorInline]
+
 
     def display_genres(self, obj):
         genres = obj.genres.all()[:3]
@@ -42,6 +51,18 @@ class MovieAdmin(admin.ModelAdmin):
             kwargs['queryset'] = Genre.objects.order_by('name')
         return super().formfield_for_manytomany(db_field, request, **kwargs)
     # do the same later for movies, alphabetize movies for review model admin
+    
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'display_movies')
+    search_fields = ('name', 'movies__title')
+    inlines = [MovieInline]
+
+    def display_movies(self, obj):
+        movies = obj.movies.all()[:3]
+        return ', '.join([movie.title for movie in movies]) + ('...' if len(movies) > 3 else ' ')
+    display_movies.short_description = 'Movies'
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
