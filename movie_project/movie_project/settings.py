fee40a5d210f4c97378ea618eca9b3d504f6967d
RoelVillaluz <roelo.villaluz@gmail.com>
2024-08-15 09:11:48 +0800

    added social media login buttons and error message to login form

--- movie_project/movie_project/settings.py
+++ movie_project/movie_project/settings.py
@@ -18,9 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
