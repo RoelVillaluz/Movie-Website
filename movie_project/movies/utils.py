bf5b0d4da4ecee1f0a24900e3f466026748d45b5
RoelVillaluz <roelo.villaluz@gmail.com>
2024-09-27 07:51:41 +0800

    updated get actor accolades function
    - added award name and category to dictionary for more specific awards

--- movie_project/movies/utils.py
+++ movie_project/movies/utils.py
@@ -142,16 +142,21 @@ def get_directors_and_most_popular_movies(directors, num_movies):
     return director_and_most_popular_movies
 
 def get_actor_accolades(actor):
-    actor_accolades = defaultdict(int, {'wins': 0, 'nominations': 0})
+    # Dictionary structure: {award_name: {category: {'wins': 0, 'nominations': 0}}}
+    actor_accolades = defaultdict(lambda: defaultdict(lambda: {'wins': 0, 'nominations': 0}))
 
     awards = actor.awards.all()
 
     for award in awards:
-        if award.winner:
-            actor_accolades["wins"] += 1
-        actor_accolades["nominations"] += 1
+        award_name = award.award_name
+        category = award.category
 
-    return dict(actor_accolades)
+        if award.winner:
+            actor_accolades[award_name][category]['wins'] += 1
+
+        actor_accolades[award_name][category]['nominations'] += 1
+
+    return {award_name: dict(categories) for award_name, categories in actor_accolades.items()}
 
 def get_genre_dict(genres):
     """Get a dictionary of popular genres and a random movie for each genre."""
