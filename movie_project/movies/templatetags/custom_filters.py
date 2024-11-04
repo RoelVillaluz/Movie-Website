26e010a5f380d1fc717bde5d2e0076bbca2110f1
RoelVillaluz <roelo.villaluz@gmail.com>
2024-11-04 16:40:27 +0800

    added is_actor and is_director custom template

--- movie_project/movies/templatetags/custom_filters.py
+++ movie_project/movies/templatetags/custom_filters.py
@@ -1,8 +1,18 @@
 from django import template
 
+from movies.models import Actor, Director
+
 register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
     """Retrieve an item from a dictionary by key."""
     return dictionary.get(key)
+
+@register.filter
+def is_actor(person):
+    return isinstance(person, Actor)
+
+@register.filter
+def is_director(person):
+    return isinstance(person, Director)
