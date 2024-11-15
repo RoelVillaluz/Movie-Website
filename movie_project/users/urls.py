2ec5d583a829ca3e06fb604e75fe67a8c92224b3
RoelVillaluz <roelo.villaluz@gmail.com>
2024-11-15 15:15:38 +0800

    added list detail view url

--- movie_project/users/urls.py
+++ movie_project/users/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('profile/<int:pk>', views.ProfileDetailView.as_view(), name="profile"),
     path('follow/<str:model_name>/<int:object_id>/', views.follow_content, name='follow_content'),
     path('create_list', views.CreateListView.as_view(), name='create-list'),
-    path('lists/<int:pk>', views.CustomListDetailView.as_view(), name='list-detail')
+    path('lists/<int:pk>', views.CustomListDetailView.as_view(), name='list-detail'),
 ]
